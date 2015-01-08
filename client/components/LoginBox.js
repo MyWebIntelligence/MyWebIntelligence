2046dc1444995d0c457d5355a1cea4ca5b08e132
David Bruant <bruant.d@gmail.com>
2015-01-08 10:38:54 +0100

    filling and styling TerritoireList

--- client/components/LoginBox.js
+++ client/components/LoginBox.js
@@ -28,6 +28,7 @@ var loggedInUserStub = {
                 {
                     id: 83,
                     name: "Twitter chocolatiers",
+                    description: "When separately written programs are composed so that they may cooperate, they may instead destructively interfere in unanticipated ways. These hazards limit the scale and functionality of the software systems we can successfully compose.",
                     queries: [
                         {
                             id: 36,
@@ -48,6 +49,7 @@ var loggedInUserStub = {
                 {
                     id: 369,
                     name: "Pokémon eau",
+                    description: "This dissertation presents a framework for enabling those interactions between components needed for the cooperation we intend, while minimizing the hazards of destructive interference. Great progress on the composition problem has been made within the object paradigm, chiefly in the context of sequential, single-machine programming among benign components.",
                     queries: [
                         {
                             id: 33,
@@ -70,6 +72,7 @@ var loggedInUserStub = {
                 {
                     id: 372,
                     name: "Pokémon feu",
+                    description: "We show how to extend this success to support robust composition of concurrent and potentially malicious components distributed over potentially malicious machines. We present E, a distributed, persistent, secure programming language, and CapDesk, a virus-safe desktop built in E, as embodiments of the techniques we explain.",
                     queries: [
                         {
                             id: 11,
--- client/components/TerritoiresList.js
+++ client/components/TerritoiresList.js
@@ -4,9 +4,7 @@ var React = require('react');
 
 module.exports = React.createClass({
     getInitialState: function(){
-        return {
-            openTerritoires: []
-        };
+        return {};
     },
     
     render: function(){
@@ -15,11 +13,39 @@ module.exports = React.createClass({
         var self = this;
         
         console.log('props', props);
+
+        /*
+            <ul class="territoires">
+                <li>
+                    <a href="">
+                        <div class="name">fliquepluch</div>
+                        <div class="description">mjdfjezfjfz</div>
+                        <ul class="queries">
+                            <li>query1
+                            <li>query2
+                        </ul>
+                    </a>
+                    <button class="delete"></button>
+                <li>
+            </ul>
         
-        return React.DOM.ul({}, props.territoires.map(function(t){
-            return React.DOM.li({
-                className: "territoire"
-            }, t.name);
+        */
+        
+        return React.DOM.ul({className: "territoires"}, props.territoires.map(function(t){
+            return React.DOM.li({}, [
+                React.DOM.a({
+                    href: "TODO",
+                    onClick: function(e){
+                        e.preventDefault();
+                    }
+                }, [
+                    React.DOM.div({className: "name"}, t.name),
+                    React.DOM.div({className: "description"}, t.description),
+                    React.DOM.ul({className: "queries"}, t.queries.map(function(q){
+                        return React.DOM.li({}, q.name);
+                    })),
+                ])
+            ]);
         }));
     }
 });

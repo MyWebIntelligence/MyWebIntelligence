2084f1c2c6866e4bcb9053b624dc0f80acc277e1
David Bruant <bruant.d@gmail.com>
2015-01-15 15:06:14 +0100

    It's now possible to edit an existing Territoire

--- client/components/TerritoireListItem.js
+++ client/components/TerritoireListItem.js
@@ -3,6 +3,7 @@
 var React = require('react');
 
 var QueryForm = require('./QueryForm');
+var TerritoireForm = require('./TerritoireForm');
 
 
 /*
@@ -29,82 +30,129 @@ module.exports = React.createClass({
         
         var t = props.territoire;
         
-        return React.DOM.li({}, [
-            React.DOM.a({
-                href: "TODO",
-                onClick: function(e){
-                    e.preventDefault();
-                }
-            }, [
-                React.DOM.h1({}, t.name),
-                React.DOM.p({className: "description"}, t.description),
-            ]),
-            React.DOM.span({
-                style: {
-                    display: "inline"
-                }
-            }, "Queries: "),
-            React.DOM.ul({className: "queries"}, t.queries.map(function(q){
-                return React.DOM.li({
-                    className: state.openQueryForms.has(q.id) ? 'open' : ''
-                }, [
-                    React.DOM.button({
-                        onClick: function(e){
-                            if(state.openQueryForms.has(q.id))
-                                state.openQueryForms.delete(q.id);
-                            else
-                                state.openQueryForms.add(q.id);
+        var children;
+        
+        if(state.editMode){
+            children = [ TerritoireForm({
+                territoire: t,
+                onSubmit: function(formData){
+                    var keysWithChange = Object.keys(formData).filter(function(k){
+                        return t[k] !== formData[k];
+                    });
+                    
+                    if(keysWithChange.length >= 1){
+                        keysWithChange.forEach(function(k){
+                            t[k] = formData[k];
+                        });
 
-                            self.setState({openQueryForms: state.openQueryForms});
-                        }
-                    }, q.name),
-                    state.openQueryForms.has(q.id) ? QueryForm({
-                        query: q,
-                        onSubmit: function(formData){
-                            var keysWithChange = Object.keys(formData).filter(function(k){
-                                return q[k] !== formData[k];
-                            });
-                            
-                            if(keysWithChange.length >= 1){
-                                keysWithChange.forEach(function(k){
-                                    q[k] = formData[k];
+                        // new territoire is the current one mutated at the .queries array level
+                        props.onTerritoireChange(t);
+                        
+                        self.setState({
+                            openQueryForms: state.openQueryForms,
+                            editMode: false
+                        });
+                    }
+                }
+            }) ];
+        }
+        else{
+            children = [
+                React.DOM.a({
+                    href: "TODO",
+                    onClick: function(e){
+                        e.preventDefault();
+                    }
+                }, [
+                    React.DOM.h1({}, t.name),
+                    React.DOM.p({className: "description"}, t.description),
+                ]),
+                React.DOM.button({
+                    className: 'edit',
+                    onClick: function(){
+                        self.setState({
+                            openQueryForms: state.openQueryForms,
+                            editMode: true
+                        })
+                    }
+                }, React.DOM.i({className: 'fa fa-pencil '}, '')),
+                React.DOM.span({
+                    style: {
+                        display: "inline"
+                    }
+                }, "Queries: "),
+                React.DOM.ul({className: "queries"}, t.queries.map(function(q){
+                    return React.DOM.li({
+                        className: state.openQueryForms.has(q.id) ? 'open' : ''
+                    }, [
+                        React.DOM.button({
+                            onClick: function(e){
+                                if(state.openQueryForms.has(q.id))
+                                    state.openQueryForms.delete(q.id);
+                                else
+                                    state.openQueryForms.add(q.id);
+
+                                self.setState({
+                                    openQueryForms: state.openQueryForms,
+                                    editMode: false
                                 });
-                                
-                                // new territoire is the current one mutated at the .queries array level
-                                props.onTerritoireChange(t);
                             }
-                            
-                            // close the form UI in all cases
-                            state.openQueryForms.delete(q.id);
-                            self.setState({openQueryForms: state.openQueryForms});
-                        }
-                    }) : undefined
-                ]);
-            }).concat([
-                React.DOM.li({
-                    className: 'add' + (state.openQueryForms.has('+') ? ' open' : '')
-                }, [
-                    React.DOM.button({
-                        onClick: function(e){
-                            if(state.openQueryForms.has('+'))
-                                state.openQueryForms.delete('+');
-                            else
-                                state.openQueryForms.add('+');
+                        }, q.name),
+                        state.openQueryForms.has(q.id) ? QueryForm({
+                            query: q,
+                            onSubmit: function(formData){
+                                var keysWithChange = Object.keys(formData).filter(function(k){
+                                    return q[k] !== formData[k];
+                                });
 
-                            self.setState({openQueryForms: state.openQueryForms});
-                        }
-                    }, '+'),
-                    state.openQueryForms.has('+') ? QueryForm({
-                        onSubmit: function(formData){
-                            props.createQuery(formData);
-                            
-                            // close the form UI in all cases
-                            state.openQueryForms.delete('+');
-                            self.setState({openQueryForms: state.openQueryForms});
-                        }
-                    }) : undefined
-                ])
-            ]))
-        ]);
+                                if(keysWithChange.length >= 1){
+                                    keysWithChange.forEach(function(k){
+                                        q[k] = formData[k];
+                                    });
+
+                                    // new territoire is the current one mutated at the .queries array level
+                                    props.onTerritoireChange(t);
+                                }
+
+                                // close the form UI in all cases
+                                state.openQueryForms.delete(q.id);
+                                self.setState({
+                                    openQueryForms: state.openQueryForms,
+                                    editMode: false
+                                });
+                            }
+                        }) : undefined
+                    ]);
+                }).concat([
+                    React.DOM.li({
+                        className: 'add' + (state.openQueryForms.has('+') ? ' open' : '')
+                    }, [
+                        React.DOM.button({
+                            onClick: function(e){
+                                if(state.openQueryForms.has('+'))
+                                    state.openQueryForms.delete('+');
+                                else
+                                    state.openQueryForms.add('+');
+
+                                self.setState({openQueryForms: state.openQueryForms});
+                            }
+                        }, '+'),
+                        state.openQueryForms.has('+') ? QueryForm({
+                            onSubmit: function(formData){
+                                props.createQuery(formData);
+
+                                // close the form UI in all cases
+                                state.openQueryForms.delete('+');
+                                self.setState({openQueryForms: state.openQueryForms});
+                            }
+                        }) : undefined
+                    ])
+                ]))
+            ]
+        }
+        
+        
+        
+        return React.DOM.li({}, children);
     }
 });

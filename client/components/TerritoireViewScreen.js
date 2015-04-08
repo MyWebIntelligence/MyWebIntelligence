7e062cadf665a81d0396e28c89a9f7d518c6f52c
David Bruant <bruant.d@gmail.com>
2015-04-08 16:23:20 +0200

    adding tabs and improving style of territoire view

--- client/components/TerritoireViewScreen.js
+++ client/components/TerritoireViewScreen.js
@@ -2,6 +2,8 @@
 
 var React = require('react');
 
+var Tabs = require('react-tabs-component');
+
 var Header = require('./Header');
 
 /*
@@ -35,22 +37,50 @@ module.exports = React.createClass({
             
             React.DOM.main({className: 'territoire'}, [
                 //React.DOM.h1({}, ),
-                React.DOM.h1({className: 'exports'}, [
-                    "Territoire "+territoire.name,
-                    React.DOM.a({href: "/territoire/"+territoire.id+"/expressions.csv"}, 'Download Pages CSV'),
-                    React.DOM.a({href: "/territoire/"+territoire.id+"/expressions.gexf"}, 'Download Pages GEXF'),
-                    React.DOM.a({href: "/territoire/"+territoire.id+"/domains.gexf"}, 'Download Domains GEXF')
+                React.DOM.h1({}, [
+                    "Territoire "+territoire.name
                 ]),
                 
-                territoire.resultList ? React.DOM.ul({className: 'result-list'}, territoire.resultList.map(function(r){
-                    return React.DOM.li({}, [
-                        React.DOM.a({ href: r.url, target: '_blank' }, [
-                            React.DOM.h3({}, r.title),
-                            React.DOM.h4({}, r.url)
-                        ]),
-                        React.DOM.div({ className: 'excerpt' }, r.excerpt)
-                    ]);
-                })) : undefined
+                React.DOM.div({className: 'tabs-and-exports'}, [
+                    new Tabs({
+                        defaultTabNum: 0,
+                        tabNames: ['Pages', 'Domains'],
+                        classPrefix: 'tabs-'
+                    }, [
+                        // Pages tab content
+                        territoire.resultList ? React.DOM.ul({className: 'result-list'}, territoire.resultList.map(function(r){
+                            return React.DOM.li({}, [
+                                React.DOM.a({ href: r.url, target: '_blank' }, [
+                                    React.DOM.h3({}, r.title),
+                                    React.DOM.h4({}, r.url)
+                                ]),
+                                React.DOM.div({ className: 'excerpt' }, r.excerpt)
+                            ]);
+                        })) : undefined,
+                        // Domains tab content
+                        territoire.resultList ? React.DOM.ul({className: 'result-list'}, territoire.resultList.slice(0, 2).map(function(r){
+                            return React.DOM.li({}, [
+                                React.DOM.a({ href: r.url, target: '_blank' }, [
+                                    React.DOM.h3({}, r.title),
+                                    React.DOM.h4({}, r.url)
+                                ]),
+                                React.DOM.div({ className: 'excerpt' }, r.excerpt)
+                            ]);
+                        })) : undefined
+                    ]),
+                    
+                    React.DOM.div({className: 'exports'}, [
+                        React.DOM.a({href: "/territoire/"+territoire.id+"/expressions.csv"}, 'Download Pages CSV'),
+                        React.DOM.a({href: "/territoire/"+territoire.id+"/expressions.gexf"}, 'Download Pages GEXF'),
+                        React.DOM.a({href: "/territoire/"+territoire.id+"/domains.gexf"}, 'Download Domains GEXF')
+                    ])
+                
+                ])
+                
+                
+                
+                
+                
             ])
         
         ]);

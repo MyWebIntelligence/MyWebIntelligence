aaf8aedbc84a7cf919b4a791ea9d236c4ea1cfa6
David Bruant <bruant.d@gmail.com>
2015-04-23 11:20:56 +0200

    graph exports with shallow nodes

--- postgresDB/dropAllTables.js
+++ postgresDB/dropAllTables.js
@@ -7,7 +7,7 @@ var databaseP = require('./databaseClientP');
 var dropTableScript = fs.readFileSync( require.resolve('./dropAllTables.sql') ).toString();
 
 module.exports = function(){
-    console.warn('WARNING! Dropping all tables!');
+    console.warn('\n\t=====\n\nWARNING! Dropping all tables!\n\n\t=====\n');
     
     return new Promise(function(resolve, reject){
         databaseP.then(function(db){

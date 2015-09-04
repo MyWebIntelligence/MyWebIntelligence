57818cb29894cd58eafe2f47f492042d336f75b9
David Bruant <bruant.d@gmail.com>
2015-09-04 17:04:43 +0200

    bring annotation tasks back. There was a duplicate key problem when creating annotations which led to tasks not being created. Annotations are now created as part of createOrFindResourceForTerritoire.js

--- server/createResourceTasks.js
+++ server/createResourceTasks.js
@@ -21,32 +21,25 @@ module.exports = function(resourceIds, options){
         // resourceIds.toJSON().map, then socialSignalTypes.map allows to "shuffle" tasks so that 
         // different services to get data are interrogated in ~Round-Robin fashion
     ].concat(resourceIdsArray.map(function(rid){
-        return database.Annotations.create({
-            resource_id: rid,
-            territoire_id: territoireId
-        })
-            .then(function(){            
-                return Promise.all(socialSignalTypes.map(function(type){
-                    return new Promise(function(resolve, reject){
-                        // Need to put space between same annotation types tasks (to prevent IP ban by services)
-                        // Need to put space between resources because updates are race-y and lose information
-                        // Schedule annotation task for later, so the task is performed later in hope that it will
-                        // prevent race conditions when updating the annotation.
-                        // Race conditions can happen for automated annotations (which isn't a big deal 
-                        // since it's cheap to redo them)
-                        // Race conditions can also happen in collision with human annotations which is a BIG deal 
-                        // but unlikely enough to be considered acceptable for now.
-                        setTimeout(function(){
-                            database.AnnotationTasks.createTasksTodo(rid, territoireId, type)
-                                .then(resolve)
-                                .catch(function(err){
-                                    console.error('annotation create error', err, err.stack);
-                                    reject(err)
-                                });  
-                        }, Math.round(Math.random() * resourceIdsArray.length * socialSignalTypes.length));
-                    })
-                }));
-            });
-        
+        return Promise.all(socialSignalTypes.map(function(type){
+            return new Promise(function(resolve, reject){
+                // Need to put space between same annotation types tasks (to prevent IP ban by services)
+                // Need to put space between resources because updates are race-y and lose information
+                // Schedule annotation task for later, so the task is performed later in hope that it will
+                // prevent race conditions when updating the annotation.
+                // Race conditions can happen for automated annotations (which isn't a big deal 
+                // since it's cheap to redo them)
+                // Race conditions can also happen in collision with human annotations which is a BIG deal 
+                // but unlikely enough to be considered acceptable for now.
+                setTimeout(function(){
+                    database.AnnotationTasks.createTasksTodo(rid, territoireId, type)
+                        .then(resolve)
+                        .catch(function(err){
+                            console.error('annotation create error', err, err.stack);
+                            reject(err)
+                        });  
+                }, Math.round(Math.random() * resourceIdsArray.length * socialSignalTypes.length));
+            })
+        }));
     })));
 };

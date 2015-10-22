526d5e147543e10afe52172a7bb8f73e3dec8085
David Bruant <bruant.d@gmail.com>
2015-10-22 14:13:44 +0200

    reducing log noise

--- database/getTerritoireScreenData.js
+++ database/getTerritoireScreenData.js
@@ -23,12 +23,12 @@ module.exports = function getTerritoireScreenData(territoireId){
         }));
     });
 
-    console.time('getTerritoireResourceGraph')
+    //console.time('getTerritoireResourceGraph')
     var abstractPageGraphP = getTerritoireResourceGraph(territoireId)
         .then(function(res){
             return res.graph;
         });
-    abstractPageGraphP.then(console.timeEnd.bind(console, 'getTerritoireResourceGraph'))
+    //abstractPageGraphP.then(console.timeEnd.bind(console, 'getTerritoireResourceGraph'))
 
     var expressionByIdP = abstractPageGraphP
         .then(getGraphExpressions)
@@ -41,10 +41,10 @@ module.exports = function getTerritoireScreenData(territoireId){
 
     var annotationByResourceIdP = abstractPageGraphP
         .then(function(graph){
-            console.time('fetching annotations');
+            //console.time('fetching annotations');
             return database.complexQueries.getGraphAnnotations(graph, territoireId);
         });
-    annotationByResourceIdP.then(console.timeEnd.bind(console, 'fetching annotations'))
+    //annotationByResourceIdP.then(console.timeEnd.bind(console, 'fetching annotations'))
 
     var expressionDomainsByIdP = database.complexQueries.getTerritoireExpressionDomains(territoireId)
         .then(function(expressionDomains){            
@@ -57,8 +57,8 @@ module.exports = function getTerritoireScreenData(territoireId){
     // timing of this query will make the values certainly out-of-sync with when 
     var progressIndicatorsP = database.complexQueries.getProgressIndicators(territoireId);
 
-    console.time('getProgressIndicators');
-    progressIndicatorsP.then(console.timeEnd.bind(console, 'getProgressIndicators'))
+    //console.time('getProgressIndicators');
+    //progressIndicatorsP.then(console.timeEnd.bind(console, 'getProgressIndicators'))
 
     console.time('all data');
     return Promise.all([
